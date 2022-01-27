aff774316a98e9545ed93b67780bb4990ac906af
daegeun <daegeunkim92@gmail.com>
2022-01-27 21:59:06 +0900

    진입화면 생성 - 애니메이션 위젯 사용 - TickerProvider를 사용하여 자동으로 애니메이션이 작동하도록 함 - 이미지가 전체화면을 차지하도록 개발

--- lib/front_door_widget.dart
+++ lib/front_door_widget.dart
@@ -1,17 +1,49 @@
 import 'package:flutter/material.dart';
 
-class FrontDoorWidget extends StatelessWidget {
+class FrontDoorWidget extends StatefulWidget {
   const FrontDoorWidget({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _FrontDoorWidgetState();
+}
+
+class _FrontDoorWidgetState extends State<FrontDoorWidget> with TickerProviderStateMixin {
+  _FrontDoorWidgetState();
+
+  // late : run lazily the first time the field is accessed
+  // running time에 _controller가 사용되는 시점에 초기화가 된다는 것을 의미한다.
+  // vsync: this는 TickerProviderStateMixin를 넣어주는 것을 의미한다. TickerProviderStateMixin는 타이머이며
+  // 한마디로 타이머가 객체 생성 시점이 아닌 build시점까지 초기화되는 것을 기다려준다는 것으로 이해했다.
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: 500),
+    vsync: this,
+  )..forward();
+
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeIn,
+  );
+  
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     // 전체를 가득 채우는 방법
-    return const Scaffold(
-      body: Image(
-        image: AssetImage('assets/images/front-door.png'),
-        fit: BoxFit.cover,
-        height: double.infinity,
-        width: double.infinity,
+    return Scaffold(
+      body: Center(
+        child: FadeTransition(
+          child: const Image(
+            image: AssetImage('assets/images/front-door.png'),
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          opacity: _animation,
+        ),
       ),
     );
   }
